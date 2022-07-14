b1ae141a2c12e116096ca3833a76bafaf4ccd8cb
Rafael Cardoso <51007688+LeafarOne@users.noreply.github.com>
2022-07-14 00:41:53 -0300

    Update web.pp

--- manifests/web.pp
+++ manifests/web.pp
@@ -1,23 +1,17 @@
 exec { "apt-update":
   command => "/usr/bin/apt-get update"
 }
-
-package { ["openjdk-7-jre", "tomcat7"]:
+package { ["openjdk-7-jre", "tomcat7", "mysql-server"]:
     ensure => installed,
     require => Exec["apt-update"]
-}
-package { 'unzip':
-  ensure => installed,
-  require => Exec["apt-update"]
 }
 service { "tomcat7":
     ensure => running,
     enable => true,
     hasstatus => true,
     hasrestart => true,
-    require => Package["tomcat7"]
+    require => Package["tomcat7"]    
 }
-
 file { "/var/lib/tomcat7/webapps/vraptor-musicjungle.war":
     source => "/vagrant/manifests/vraptor-musicjungle.war",
     owner => "tomcat7",
@@ -26,11 +20,16 @@ file { "/var/lib/tomcat7/webapps/vraptor-musicjungle.war":
     require => Package["tomcat7"],
     notify => Service["tomcat7"]
 }
-file { "/var/lib/tomcat7/webapps/musicjungle.war":
-    source => "/vagrant/manifests/vraptor-musicjungle.war",
-    owner => "tomcat7",
-    group => "tomcat7",
-    mode => 0644,
-    require => Package["tomcat7"],
-    notify => Service["tomcat7"]
+service { "mysql":
+    ensure => running,
+    enable => true,
+    hasstatus => true,
+    hasrestart => true,
+    require => Package["mysql-server"]
+}
+exec { "musicjungle":
+    command => "mysqladmin -uroot create musicjungle",
+    unless => "mysql -u root musicjungle",
+    path => "/usr/bin",
+    require => Service["mysql"]
 }
