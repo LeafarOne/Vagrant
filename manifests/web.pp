37f6688b0cd6898b638b827486b8f33c8ed4374b
Rafael Cardoso <51007688+LeafarOne@users.noreply.github.com>
2022-07-18 18:29:25 -0300

    Update web.pp

--- manifests/web.pp
+++ manifests/web.pp
@@ -1,52 +1,53 @@
 exec { "apt-update":
-	command => "/usr/bin/apt-get update"
+    command => "/usr/bin/apt-get update"
 }
 
-package { ["vim", "openjdk-8-jre", "tomcat8", "mariadb-server"]:
-	ensure => installed,
-	require => Exec["apt-update"]
+package { ["openjdk-7-jre", "tomcat7", "mysql-server"]:
+    ensure => installed,
+    require => Exec["apt-update"]
 }
 
-service { "mariadb":
+service { "tomcat7":
     ensure => running,
     enable => true,
     hasstatus => true,
     hasrestart => true,
-    require => Package["mariadb-server"]
+    require => Package["tomcat7"]    
+}
+
+file { "/var/lib/tomcat7/webapps/vraptor-musicjungle.war":
+    source => "/vagrant/manifests/vraptor-musicjungle.war",
+    owner => "tomcat7",
+    group => "tomcat7",
+    mode => 0644,
+    require => Package["tomcat7"],
+    notify => Service["tomcat7"]
+}
+
+service { "mysql":
+    ensure => running,
+    enable => true,
+    hasstatus => true,
+    hasrestart => true,
+    require => Package["mysql-server"]
 }
 
 exec { "musicjungle":
     command => "mysqladmin -uroot create musicjungle",
     unless => "mysql -u root musicjungle",
     path => "/usr/bin",
-    require => Service["mariadb"]
-}
-
-exec { "user_mariadb":
-    command => "/bin/echo \"CREATE USER 'admin'@'localhost';GRANT USAGE on *.* TO 'admin'@'localhost';GRANT ALL privileges ON musicjungle.* TO  'admin'@'localhost';FLUSH PRIVILEGES;\" |  /usr/bin/mysql -u root",
-    require => Exec["musicjungle"]
-}
-
-service { "tomcat8":
-	ensure => running,
-	enable => true,
-	hasstatus => true,
-	hasrestart => true,
-	require => Package["tomcat8"]
-}
-
-file { "/var/lib/tomcat8/webapps/vraptor-musicjungle.war":
-	source => "/vagrant/manifests/vraptor-musicjungle.war",
-	owner => "tomcat8",
-	group => "tomcat8",
-	mode => "0644",
-	require => Package["tomcat8"],
-	notify => Service["tomcat8"]
+    require => Service["mysql"]
 }
 
 file_line { "production":
-    path => "/etc/default/tomcat8",
+    file => "/etc/default/tomcat7",
     line => "JAVA_OPTS=\"\$JAVA_OPTS -Dbr.com.caelum.vraptor.environment=production\"",
-    require => Package["tomcat8"],
-    notify => Service["tomcat8"]
+    require => Package["tomcat7"],
+    notify => Service["tomcat7"]
+}
+
+define file_line($file, $line) {
+    exec { "/bin/echo '$line' >> '$file'":
+        unless => "/bin/grep -qfx '$(line)' '$(file)'"
+    }
 }
