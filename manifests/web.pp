7bf5586bd355ecfed73c9360a9dd9fa21b6804c6
Rafael Cardoso <51007688+LeafarOne@users.noreply.github.com>
2022-07-18 18:25:36 -0300

    Update web.pp

--- manifests/web.pp
+++ manifests/web.pp
@@ -1,10 +1,12 @@
 exec { "apt-update":
-  command => "/usr/bin/apt-get update"
+    command => "/usr/bin/apt-get update"
 }
+
 package { ["openjdk-7-jre", "tomcat7", "mysql-server"]:
     ensure => installed,
     require => Exec["apt-update"]
 }
+
 service { "tomcat7":
     ensure => running,
     enable => true,
@@ -12,6 +14,7 @@ service { "tomcat7":
     hasrestart => true,
     require => Package["tomcat7"]    
 }
+
 file { "/var/lib/tomcat7/webapps/vraptor-musicjungle.war":
     source => "/vagrant/manifests/vraptor-musicjungle.war",
     owner => "tomcat7",
@@ -20,6 +23,7 @@ file { "/var/lib/tomcat7/webapps/vraptor-musicjungle.war":
     require => Package["tomcat7"],
     notify => Service["tomcat7"]
 }
+
 service { "mysql":
     ensure => running,
     enable => true,
@@ -27,9 +31,23 @@ service { "mysql":
     hasrestart => true,
     require => Package["mysql-server"]
 }
+
 exec { "musicjungle":
     command => "mysqladmin -uroot create musicjungle",
     unless => "mysql -u root musicjungle",
     path => "/usr/bin",
     require => Service["mysql"]
+}
+
+file_line { "production":
+    file => "/etc/default/tomcat7",
+    line => "JAVA_OPTS=\"\$JAVA_OPTS -Dbr.com.caelum.vraptor.environment=production\"",
+    require => Package["tomcat7"],
+    notify => Service["tomcat7"]
+}
+
+define file_line($file, $line) {
+    exec { "/bin/echo '$line' >> '$file'":
+        unless => "/bin/grep -qfx '$(line)' '$(file)'"
+    }
 }
